2f18c6a0e225261f2fb44612c959665d0fca7916
LuisSalazar02 <42986993+LuisSalazar02@users.noreply.github.com>
2021-09-09 14:07:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,20 +8,6 @@ var flag = false;
 
  class App extends Component {
 
-  handleChange(){
-    if(flag == false){
-      document.getElementById('song').play();
-      document.getElementById('icon').style.display = "block";
-      document.getElementById('icon').classList.add("anime");
-      flag = true;
-    }else{
-      document.getElementById('song').pause();
-      document.getElementById('icon').style.display = "none";
-      document.getElementById('icon').classList.remove('anime');
-      flag = false;
-    }
-  }
-
   render(){
     return(
       <div className="App">
@@ -35,7 +21,6 @@ var flag = false;
           </div>
           <div class = "container">
             <img src={image}></img>
-            <input type="checkbox" onChange={this.handleChange}></input>
             <div class = "heart" id = "icon">
                 <FontAwesomeIcon icon="heart" color="red"/>
             </div>
